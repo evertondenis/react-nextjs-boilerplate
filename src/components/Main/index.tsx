eb775e895e992341d0c75db83b68be7e79a2c46d
Everton Denis <evertondenis@gmail.com>
2020-07-23 18:49:21 -0300

    config styled-components and install and config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import { Wrapper, Logo, Title, Description, Illustration } from './styled'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <Wrapper>
+    <Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <Title>{title}</Title>
+    <Description>{description}</Description>
+    <Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </Wrapper>
 )
 
 export default Main
